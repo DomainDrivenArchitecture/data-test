cca5838fceb793dd6fe46f28795cf8b6cfc4f471
jem <michael.jerger@meissa-gmbh.de>
2019-05-16 08:20:55 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # data-test
+[![Clojars Project](https://img.shields.io/clojars/v/dda/data-test.svg)](https://clojars.org/dda/data-test)
+[![Build Status](https://travis-ci.org/DomainDrivenArchitecture/data-test.svg?branch=master)](https://travis-ci.org/DomainDrivenArchitecture/data-test)
+
+[![Slack](https://img.shields.io/badge/chat-clojurians-green.svg?style=flat)](https://clojurians.slack.com/messages/#dda-pallet/) | [<img src="https://domaindrivenarchitecture.org/img/meetup.svg" width=50 alt="DevOps Hacking with Clojure Meetup"> DevOps Hacking with Clojure](https://www.meetup.com/de-DE/preview/dda-pallet-DevOps-Hacking-with-Clojure) | [Website & Blog](https://domaindrivenarchitecture.org)
+
+
 framework for data-driven tests
---
+++ project.clj
@@ -0,0 +1,37 @@
+(defproject dda/data-test "0.1.0-SNAPSHOT"
+  :description "a data driven testing framework."
+  :url "https://domaindrivenarchitecture.org"
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
+  :dependencies []
+  :source-paths ["main/src"]
+  :resource-paths ["main/resources"]
+  :repositories [["snapshots" :clojars]
+                 ["releases" :clojars]]
+  :deploy-repositories [["snapshots" :clojars]
+                        ["releases" :clojars]]
+  :plugins [[lein-tools-deps "0.4.5"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :profiles {:dev {:source-paths ["test/src"]
+                   :resource-paths ["test/resources"]
+                   :dependencies
+                   [[org.clojure/test.check "0.10.0-alpha3"]
+                    ;[expectations "2.2.0-SNAPSHOT"]]
+                   :plugins
+                   [[lein-sub "0.3.0"]                    
+                   :leiningen/reply
+                   {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
+                    :exclusions [commons-logging]}
+                   :repl-options {:init-ns data-test}}
+             :test {:test-paths ["test/src"]
+                    :resource-paths ["test/resources"]
+                    :dependencies [;[expectations "2.2.0-SNAPSHOT"]]}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
+  :local-repo-classpath true)
