d422411e867a66228f31370a11c1deb0fa6be9d5
jem <michael.jerger@meissa-gmbh.de>
2019-05-17 09:37:56 +0200

    project might now run ...

--- project.clj
+++ project.clj
@@ -12,6 +12,7 @@
                         ["releases" :clojars]]
   :plugins [[lein-tools-deps "0.4.5"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
   :profiles {:dev {:source-paths ["test/src"]
                    :resource-paths ["test/resources"]
                    :dependencies
