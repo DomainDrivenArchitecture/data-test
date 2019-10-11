bdc82c1bc242e97936d4d02652b73d2754f4456f
jem <michael.jerger@meissa-gmbh.de>
2019-10-11 13:27:03 +0200

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/data-test "0.1.1-SNAPSHOT"
+(defproject dda/data-test "0.1.1"
   :description "a data driven testing framework."
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
